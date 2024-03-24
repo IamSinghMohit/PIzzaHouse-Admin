3f5b9f4c6f0ed73bb27b8dd6355d27200730ab3f
SinghMohit <nishadamar989@gmail.com>
2024-03-24 22:10:41 +0530

    change heading in faq page

--- src/modules/faq/index.tsx
+++ src/modules/faq/index.tsx
@@ -8,26 +8,25 @@ function FaqPage({}: Props) {
             <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20 bg-white border-2 shadow-sm rounded-lg pb-10">
                 <div className="font-bold py-3 text-xl">FAQ page</div>
                 <div>
-                    <div className="my-2">
+                    <div className="my-2 text-gray-600">
+                        🔥There are two separte projects. It is one of them,
                         <a
-                            href={
-                                import.meta.env
-                                    .VITE_CLOUDINARY_PERSONAL_SITE_URL
-                            }
+                            href="https://mohit-nishad.vercel.app"
                             target="_blank"
-                            className="text-primaryOrange underline"
+                            className="text-blue-800 hover:underline"
                         >
-                            know more about me 💡
+                            {" "}
+                            wanna know more{" "}
                         </a>
                     </div>
                     <div className="max-h-[315px] md:w-[560px] mb-10 mx-auto">
-                        <div className="text-gray-700">
+                        <h5 className="text-black font-bold text-xl">
                             Prefer watching a video😇
-                        </div>
+                        </h5>
                         <div className="relative pb-[56.25%] max-w-[560px] w-full h-0 max-h-[315px]">
                             <iframe
                                 className="w-full h-full absolute top-0 left-0 max-h-[315px] max-w-[560px]"
-                                src="https://www.youtube.com/embed/QhWyXj2bQFc?si=dlOpTrL0SjlG__eC"
+                                src="https://www.youtube.com/embed/7N5AY56IN3s?si=clU-lmo0rdgeoXFm"
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
